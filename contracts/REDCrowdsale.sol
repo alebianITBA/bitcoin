87ee66e60adaefd6b339172d76dfc393ff6de0e8
alebian <alebezdjian@gmail.com>
2017-11-10 14:24:54 -0300

    REDCrowdsale.sol v1.0

--- contracts/REDCrowdsale.sol
+++ contracts/REDCrowdsale.sol
@@ -3,166 +3,35 @@ pragma solidity ^0.4.11;
 import "zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/FinalizableCrowdsale.sol";
-import "./REDContinuousSale.sol";
 import "./REDToken.sol";
 
 contract REDCrowdsale is CappedCrowdsale, FinalizableCrowdsale {
-
     uint256 public constant TOTAL_SHARE = 100;
     uint256 public constant CROWDSALE_SHARE = 80;
     uint256 public constant FOUNDATION_SHARE = 20;
 
-    // price at which whitelisted buyers will be able to buy tokens
-    uint256 public preferentialRate;
-
-    // customize the rate for each whitelisted buyer
-    mapping (address => uint256) public buyerRate;
-
-    // initial rate at which tokens are offered
-    uint256 public initialRate;
-
-    // end rate at which tokens are offered
-    uint256 public endRate;
-
-    // continuous crowdsale contract
-    REDContinuousSale public continuousSale;
-
-    event WalletChange(address wallet);
-
-    event PreferentialRateChange(address indexed buyer, uint256 rate);
-
-    event InitialRateChange(uint256 rate);
-
-    event EndRateChange(uint256 rate);
-
     function REDCrowdsale(
-        uint256 _startBlock,
-        uint256 _endBlock,
-        uint256 _initialRate,
-        uint256 _endRate,
-        uint256 _preferentialRate,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _rate,
         address _wallet
     )
-        CappedCrowdsale(86206 ether)
+        CappedCrowdsale(16666 ether)
         FinalizableCrowdsale()
-        Crowdsale(_startBlock, _endBlock, _initialRate, _wallet)
+        Crowdsale(_startTime, _endTime, _rate, _wallet)
     {
-        require(_initialRate > 0);
-        require(_endRate > 0);
-        require(_preferentialRate > 0);
-
-        initialRate = _initialRate;
-        endRate = _endRate;
-        preferentialRate = _preferentialRate;
-
-        continuousSale = createContinuousSaleContract();
-
         REDToken(token).pause();
     }
 
-    function createTokenContract() internal returns(MintableToken) {
+    function createTokenContract() internal returns (MintableToken) {
         return new REDToken();
     }
 
-    function createContinuousSaleContract() internal returns(REDContinuousSale) {
-        return new REDContinuousSale(rate, wallet, token);
+    function hasEnded() public constant returns (bool) {
+        return isFinalized || super.hasEnded();
     }
 
-    function setBuyerRate(address buyer, uint256 rate) onlyOwner public {
-        require(rate != 0);
-        require(isWhitelisted(buyer));
-        require(block.number < startBlock);
-
-        buyerRate[buyer] = rate;
-
-        PreferentialRateChange(buyer, rate);
-    }
-
-    function setInitialRate(uint256 rate) onlyOwner public {
-        require(rate != 0);
-        require(block.number < startBlock);
-
-        initialRate = rate;
-
-        InitialRateChange(rate);
-    }
-
-    function setEndRate(uint256 rate) onlyOwner public {
-        require(rate != 0);
-        require(block.number < startBlock);
-
-        endRate = rate;
-
-        EndRateChange(rate);
-    }
-
-    function getRate() internal returns(uint256) {
-        // some early buyers are offered a discount on the crowdsale price
-        if (buyerRate[msg.sender] != 0) {
-            return buyerRate[msg.sender];
-        }
-
-        // whitelisted buyers can purchase at preferential price before crowdsale ends
-        if (isWhitelisted(msg.sender)) {
-            return preferentialRate;
-        }
-
-        // otherwise compute the price for the auction
-        uint256 elapsed = block.number - startBlock;
-        uint256 rateRange = initialRate - endRate;
-        uint256 blockRange = endBlock - startBlock;
-
-        return initialRate.sub(rateRange.mul(elapsed).div(blockRange));
-    }
-
-    // low level token purchase function
-    function buyTokens(address beneficiary) payable {
-        require(beneficiary != 0x0);
-        require(validPurchase());
-
-        uint256 weiAmount = msg.value;
-        uint256 updatedWeiRaised = weiRaised.add(weiAmount);
-
-        uint256 rate = getRate();
-        // calculate token amount to be created
-        uint256 tokens = weiAmount.mul(rate);
-
-        // update state
-        weiRaised = updatedWeiRaised;
-
-        token.mint(beneficiary, tokens);
-        TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-
-        forwardFunds();
-    }
-
-    function setWallet(address _wallet) onlyOwner public {
-        require(_wallet != 0x0);
-        wallet = _wallet;
-        continuousSale.setWallet(_wallet);
-        WalletChange(_wallet);
-    }
-
-    function unpauseToken() onlyOwner {
-        require(isFinalized);
-        REDToken(token).unpause();
-    }
-
-    function pauseToken() onlyOwner {
-        require(isFinalized);
-        REDToken(token).pause();
-    }
-
-
-    function beginContinuousSale() onlyOwner public {
-        require(isFinalized);
-
-        token.transferOwnership(continuousSale);
-
-        continuousSale.start();
-        continuousSale.transferOwnership(owner);
-    }
-
+    // After the crowdsale is finished, mint foundation tokens
     function finalization() internal {
         uint256 totalSupply = token.totalSupply();
         uint256 finalSupply = TOTAL_SHARE.mul(totalSupply).div(CROWDSALE_SHARE);
@@ -174,4 +43,13 @@ contract REDCrowdsale is CappedCrowdsale, FinalizableCrowdsale {
         // the continuous sale would not be able to proceed
     }
 
+    function unpauseToken() onlyOwner {
+        require(isFinalized);
+        REDToken(token).unpause();
+    }
+
+    function pauseToken() onlyOwner {
+        require(isFinalized);
+        REDToken(token).pause();
+    }
 }
--- contracts/REDToken.sol
+++ contracts/REDToken.sol
@@ -5,11 +5,8 @@ import "zeppelin-solidity/contracts/token/MintableToken.sol";
 import "./BurnableToken.sol";
 
 contract REDToken is BurnableToken, PausableToken, MintableToken {
-
     string public constant symbol = "RED";
-
     string public constant name = "RGB RED";
-
     uint8 public constant decimals = 18;
 
     function burn(uint256 _value) whenNotPaused public {
